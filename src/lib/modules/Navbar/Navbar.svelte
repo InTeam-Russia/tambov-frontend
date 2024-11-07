5408f796cdcb64c9c602e17bb1bfebdba64b9a30
tolstovr <robertproducts2021@gmail.com>
2024-11-07 11:45:22 +0400

    fix(Navbar): overflow and code style

--- src/lib/modules/Navbar/Navbar.svelte
+++ src/lib/modules/Navbar/Navbar.svelte
@@ -8,7 +8,7 @@
 
   let isNavToggled = false;
 
-  const toggleNav = (event: Event) => {
+  const toggleNav = () => {
     isNavToggled = !isNavToggled;
   };
 </script>
@@ -21,32 +21,38 @@
     ><Hospital />
     <span class="w-full hidden sm:inline">&nbsp;&nbsp;Педицинское&nbsp;GOVнище</span>
   </a>
-  <div class="flex flex-row items-center flex-nowrap ">
+  <div class="flex flex-row items-center flex-nowrap">
     <ul
-    class={`flex flex-row flex-wrap items-center gap-4 list-none p-4 w-[calc(100%-2rem)] md:w-auto md:p-0 fixed top-20 z-10 rounded-lg shadow-md bg-white dark:bg-zinc-900 md:bg-none md:shadow-none md:rounded-none md:relative md:top-0 ${isNavToggled ? '-translate-x-0 left-4 right-4 opacity-100' : '-left-full opacity-0'} transition-all md:translate-x-full opacity-100`}>
-    {#each navlinks as navlink}
+      class={`flex flex-row flex-wrap items-center gap-4 list-none p-4 w-[calc(100%-2rem)] md:w-auto md:p-0 fixed top-20 z-10 rounded-lg shadow-md bg-white dark:bg-zinc-900 md:bg-none md:shadow-none md:rounded-none md:relative md:top-0 ${isNavToggled ? '-translate-x-0 left-4 right-4 opacity-100' : '-left-full opacity-0'} transition-all md:translate-x-full opacity-100`}>
+      {#each navlinks as navlink}
+        <li>
+          <a href={navlink.url}>
+            <Button variant="ghost">
+              {navlink.title}
+            </Button>
+          </a>
+        </li>
+      {/each}
       <li>
-        <a href={navlink.url}>
-          <Button variant="ghost">
-            {navlink.title}
-          </Button>
-        </a>
+        <ThemeSwitcher />
       </li>
-    {/each}
-    <li>
-      <ThemeSwitcher />
-    </li>
-  </ul>
-  <Button
-    variant="outline"
-    size="icon"
-    class="md:hidden"
-    on:click={toggleNav}>
-    {#if isNavToggled}
-      <ChevronLeft />
-    {:else}
-      <ChevronRight />
-    {/if}
-  </Button>
+    </ul>
+    <Button
+      variant="outline"
+      size="icon"
+      class="md:hidden"
+      on:click={toggleNav}>
+      {#if isNavToggled}
+        <ChevronLeft />
+      {:else}
+        <ChevronRight />
+      {/if}
+    </Button>
   </div>
 </nav>
+
+<style>
+  :global(body) {
+    overflow-x: hidden;
+  }
+</style>
